9a1a79aa98126b109cba8c563b00ecccf79c186a
Fuck You <fuckyou@gmail.com>
2019-07-03 19:02:17 -0400

    Batch update

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
    
    $(document).ready(function()
    {
+      $('#wb_Heading1').addClass('visibility-hidden');
+      $("a[href*='#intro']").click(function(event)
+      {
+         event.preventDefault();
+         $('html, body').stop().animate({ scrollTop: $('#intro').offset().top }, 600, 'easeOutQuad');
+      });
+      function onScrollHeading1()
+      {
+         var $obj = $("#wb_Heading1");
+         if (!$obj.hasClass("in-viewport") && $obj.inViewPort(false))
+         {
+            $obj.addClass("in-viewport");
+            AnimateCss('wb_Heading1', 'vanish-in', 100, 1000);
+         }
+         else
+         if ($obj.hasClass("in-viewport") && !$obj.inViewPort(true))
+         {
+            $obj.removeClass("in-viewport");
+            AnimateCss('wb_Heading1', 'animate-fade-out', 0, 0);
+         }
+      }
+      if (!$('#wb_Heading1').inViewPort(true))
+      {
+         $('#wb_Heading1').addClass("in-viewport");
+      }
+      onScrollHeading1();
+      $(window).scroll(function(event)
+      {
+         onScrollHeading1();
+      });
       $("a[href*='#header']").click(function(event)
       {
          event.preventDefault();
@@ -12,11 +42,6 @@
          {
             $(this).collapse('hide');
          }
-      });
-      $("a[href*='#home']").click(function(event)
-      {
-         event.preventDefault();
-         $('html, body').stop().animate({ scrollTop: $('#wb_home').offset().top-88 }, 600, 'easeOutCirc');
       });
       $("a[href*='#services']").click(function(event)
       {
@@ -46,6 +71,17 @@
       var iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
       if (iOS)
       {
-         $('#wb_home,#wb_banner1,#wb_banner2').css('background-attachment', 'scroll');
+         $('#intro,#wb_banner1,#wb_banner2').css('background-attachment', 'scroll');
       }
+      $(window).on('resize', function()
+      {
+         ResponsiveVideo('intro-video');
+      });
+      ResponsiveVideo('intro-video');
+   });
+   
+   $(document).ready(function()
+   {
+      // move arrow outside the layer container
+      $('#wb_down-arrow').appendTo('#intro');
    });
