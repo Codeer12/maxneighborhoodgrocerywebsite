a8fd52089c16b21e7635b9d15bc353599d8ed449
Codeer12 <maxgamertv12@gmail.com>
2019-07-21 19:33:44 -0400

    Batch update

--- index.js
+++ index.js
@@ -18,25 +18,39 @@
          event.preventDefault();
          $('html, body').stop().animate({ scrollTop: $('#wb_home').offset().top-88 }, 600, 'easeOutCirc');
       });
-      $("a[href*='#services']").click(function(event)
-      {
-         event.preventDefault();
-         $('html, body').stop().animate({ scrollTop: $('#wb_services').offset().top-88 }, 600, 'easeOutCirc');
-      });
       $("a[href*='#banner1']").click(function(event)
       {
          event.preventDefault();
          $('html, body').stop().animate({ scrollTop: $('#wb_banner1').offset().top-88 }, 600, 'easeOutCirc');
       });
-      $("a[href*='#questions']").click(function(event)
+      $("a[href*='#about']").click(function(event)
       {
          event.preventDefault();
-         $('html, body').stop().animate({ scrollTop: $('#wb_questions').offset().top-88 }, 600, 'easeOutCirc');
+         $('html, body').stop().animate({ scrollTop: $('#about').offset().top-50 }, 600, 'easeInQuad');
       });
-      $("a[href*='#LayoutGrid1']").click(function(event)
+      $("a[href*='#Layer1']").click(function(event)
       {
          event.preventDefault();
-         $('html, body').stop().animate({ scrollTop: $('#wb_LayoutGrid1').offset().top-88 }, 600, 'easeOutCirc');
+         $('html, body').stop().animate({ scrollTop: $('#Layer1').offset().top-50 }, 600, 'easeInQuad');
+      });
+      function onScrollsocial()
+      {
+         var $obj = $("#wb_social");
+         if (!$obj.hasClass("in-viewport") && $obj.inViewPort(false))
+         {
+            $obj.addClass("in-viewport");
+            AnimationResume('wb_socialIcon3');
+         }
+      }
+      onScrollsocial();
+      $(window).scroll(function(event)
+      {
+         onScrollsocial();
+      });
+      $("a[href*='#links']").click(function(event)
+      {
+         event.preventDefault();
+         $('html, body').stop().animate({ scrollTop: $('#wb_links').offset().top-68 }, 600, 'easeOutCirc');
       });
       var iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
       if (iOS)
